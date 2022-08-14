55d5cfb329f4bee86f2509ad09cbdb94dc675d47
ozzy <ozzy@cydeo.com>
2022-08-14 11:56:01 -0400

    Generics

--- m1-generics/src/main/java/com/cydeo/Main.java
+++ m1-generics/src/main/java/com/cydeo/Main.java
@@ -1,9 +1,31 @@
 package com.cydeo;
 
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
 
+        ArrayList<Integer> items = new ArrayList<Integer>();  //ArrayList only accepts objects belongs to Integer class
 
+        items.add(1);
+        items.add(2);
+        items.add(3);
+        items.add(4);
+//        items.add("Apple");
+
+
+
+        printDoubled(items);
+
+
+
+
+    }
+
+    private static void printDoubled(ArrayList<Integer> items) {
+        for(Integer i : items){
+            System.out.println(i*2);
+        }
     }
 }
