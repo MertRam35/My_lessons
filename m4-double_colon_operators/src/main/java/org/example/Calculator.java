ed42d7fbb01010c492da325f067a1bae33f65bd4
MertRam35 <mert.ramadan35@gmail.com>
2023-07-13 13:43:55 +0300

    Calculator class added

---
+++ m4-double_colon_operators/src/main/java/org/example/Calculator.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class Calculator {
+
+
+    public static void finSum(int x, int y) {
+        System.out.println("Sum " + (x + y));
+    }
+
+    public static void finMultiply(int x, int y) {
+        System.out.println("Multiply " + (x * y));
+    }
+
+}
